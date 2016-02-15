de8107630b400e68e7dbfba0ceec12f59c80f8b3
dwestgate <dwestgate@usfca.edu>
2016-02-15 07:13:27 -0800

    Initial Commit

---
+++ HuffmanConverter/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  HuffmanConverter
+//
+//  Created by David Westgate on 2/15/16.
+//  Copyright © 2016 Refabricants. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
